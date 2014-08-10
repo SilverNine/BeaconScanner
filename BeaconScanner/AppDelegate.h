51595132652e38ddf7bb6b8feaad4295561fd968
SilverNine <SilverNine@SilverNineui-MacBook-Pro.local>
2014-08-10 17:55:55 +0900

    Initial Commit

---
+++ BeaconScanner/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BeaconScanner
+//
+//  Created by SilverNine on 2014. 8. 10..
+//  Copyright (c) 2014년 B-Conner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BeaconScanner/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  BeaconScanner
+//
+//  Created by SilverNine on 2014. 8. 10..
+//  Copyright (c) 2014년 B-Conner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
